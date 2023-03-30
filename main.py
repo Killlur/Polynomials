d8e83026a909a78dbe762900a95ff1167eaff861
Killlur <kavyamranjan17@gmail.com>
2023-03-30 14:37:37 +0530

    Fully done with oop, naming working

--- main.py
+++ main.py
@@ -15,42 +15,33 @@ class Polynomial:
             eqn += inlist[i] * (x ** (self.degree - i))
         return eqn
     def nameofpolynomial(self):
-        print("x\u00b2 + y\u00b2 = 2")
-        pass
-
+        inlist = self.coeffilist
+        eqn = "-" if abs(inlist[0]) != inlist[0] else ""
+        for i in range(self.degree+1):
+            eqn += f'{abs(inlist[i]) if abs(inlist[i]) !=1 else ""}x{self.__get_super(str(self.degree - i))}{"" if inlist[i+1] == (None or 0) else ("-" if abs(inlist[i+1]) != inlist[i+1] else "+")}' if (inlist[i] != 0 and self.degree - i != 0) else (f'{inlist[i]}' if inlist[i] !=0 else "")
+        return eqn
     def plotpolynomial(self):
-
 
         x = np.linspace(-100, 100)
         fig, ax = plt.subplots()
-        ax.plot(x, self.valueofpolynomial(x))
+        ax.plot(x, self.valueofpolynomial(x),label=self.nameofpolynomial())
         # ax.set(ylim=(-abs(polynomial([a] + pollist)),abs(polynomial([a] + pollist))))
+        plt.legend()
         plt.show()
 
+    def minima(self):
+        print("no calculus")
 
-def polynomial(inlist=[]):
-    x = inlist.pop(0)
-    r = len(inlist)
-    eqn = 0
-    weqn=""
-    for i in range(len(inlist)-1):
-        eqn += inlist[i]*x**(r-i)
-        weqn += str(inlist[i])+"$\mathregular{x^{2}}$"
-    print(weqn)
-    return eqn
-
-def plotpolynomial(inlist1=[]):
-    a = inlist1.pop(0)
-    pollist=[i for i in inlist1]
-    print(a,pollist)
-
-    x = np.linspace(-a,a)
-    fig,ax = plt.subplots()
-    ax.plot(x,polynomial([x] + pollist))
-    # ax.set(ylim=(-abs(polynomial([a] + pollist)),abs(polynomial([a] + pollist))))
-    plt.show()
+    # Thank you internet
+    @staticmethod
+    def __get_super(x):
+        normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-=()"
+        super_s = "ᴬᴮᶜᴰᴱᶠᴳᴴᴵᴶᴷᴸᴹᴺᴼᴾQᴿˢᵀᵁⱽᵂˣʸᶻᵃᵇᶜᵈᵉᶠᵍʰᶦʲᵏˡᵐⁿᵒᵖ۹ʳˢᵗᵘᵛʷˣʸᶻ⁰¹²³⁴⁵⁶⁷⁸⁹⁺⁻⁼⁽⁾"
+        res = x.maketrans(''.join(normal), ''.join(super_s))
+        return x.translate(res)
 
 
-quadratic = Polynomial([2,0,4])
+quadratic = Polynomial([-4,23,-1,4 , 0])
 print(quadratic.valueofpolynomial(5),quadratic.degree,quadratic.nameofpolynomial())
 quadratic.plotpolynomial()
+
