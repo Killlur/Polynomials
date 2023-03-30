547a59fe881162719bc4f121fe81fc16554417ee
Killlur <kavyamranjan17@gmail.com>
2023-03-30 16:46:01 +0530

    added y=0 when all numbers in list are 0; removed unnecessary code when plotting

--- main.py
+++ main.py
@@ -16,16 +16,19 @@ class Polynomial:
         return eqn
     def nameofpolynomial(self):
         inlist = self.coeffilist
-        eqn = "-" if abs(inlist[0]) != inlist[0] else ""
-        for i in range(self.degree+1):
-            eqn += f'{abs(inlist[i]) if abs(inlist[i]) !=1 else ""}x{self.__get_super(str(self.degree - i))}{"" if inlist[i+1] == (None) else ("-" if abs([i for i in inlist[i:] if i!=0][0]) == [i for i in inlist[i:] if i!=0][0] else "+")}' if (inlist[i] != 0 and self.degree - i != 0) else (f'{inlist[i]}' if inlist[i] !=0 else "")
+        if all((i == 0) for i in inlist):
+            eqn='y=0'
+        else:
+            eqn = "y=-" if abs(inlist[0]) != inlist[0] else "y="
+            for i in range(self.degree+1):
+                eqn += f'{abs(inlist[i]) if abs(inlist[i]) !=1 else ""}x{self.__get_super(str(self.degree - i))}{"" if inlist[i+1] == (None) else ("-" if abs([i for i in inlist[i:] if i!=0][0]) == [i for i in inlist[i:] if i!=0][0] else "+")}' if (inlist[i] != 0 and self.degree - i != 0) else (f'{inlist[i]}' if inlist[i] !=0 else "")
         return eqn
     def plotpolynomial(self):
 
-        x = np.linspace(-100, 100)
+        x = np.linspace(-1000, 1000)
         fig, ax = plt.subplots()
         ax.plot(x, self.valueofpolynomial(x),label=self.nameofpolynomial())
-        # ax.set(ylim=(-abs(polynomial([a] + pollist)),abs(polynomial([a] + pollist))))
+        ax.plot(x,Polynomial([0]).valueofpolynomial(x))
         plt.legend()
         plt.show()
 
@@ -41,7 +44,7 @@ class Polynomial:
         return x.translate(res)
 
 
-poly1 = Polynomial([-4,0,0,0,23,-1,4])
+poly1 = Polynomial([0,1,0,9,4])
 print(poly1.valueofpolynomial(5),poly1.degree,poly1.nameofpolynomial())
 poly1.plotpolynomial()
 
