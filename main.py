80c1412261b978dd4a0e96fdb831a1a364174fa0
Killlur <kavyamranjan17@gmail.com>
2023-03-30 11:50:11 +0530

    oop approach basic done, working on name

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+class Polynomial:
+    def __init__(self,coefflist : list):
+        self.degree= len(coefflist)-1
+        self.coeffilist=coefflist
+    def degreofpolynomial(self):
+        return self.degree
+
+    def valueofpolynomial(self,x):
+        inlist = self.coeffilist
+        eqn = 0
+        for i in range(self.degree+1):
+            eqn += inlist[i] * (x ** (self.degree - i))
+        return eqn
+    def nameofpolynomial(self):
+        print("x\u00b2 + y\u00b2 = 2")
+        pass
+
+    def plotpolynomial(self):
+
+
+        x = np.linspace(-100, 100)
+        fig, ax = plt.subplots()
+        ax.plot(x, self.valueofpolynomial(x))
+        # ax.set(ylim=(-abs(polynomial([a] + pollist)),abs(polynomial([a] + pollist))))
+        plt.show()
+
+
 def polynomial(inlist=[]):
-    var = inlist.pop(0)
+    x = inlist.pop(0)
     r = len(inlist)
     eqn = 0
     weqn=""
     for i in range(len(inlist)-1):
-        eqn += inlist[i]*var**(r-i)
+        eqn += inlist[i]*x**(r-i)
         weqn += str(inlist[i])+"$\mathregular{x^{2}}$"
     print(weqn)
     return eqn
@@ -24,5 +51,6 @@ def plotpolynomial(inlist1=[]):
     plt.show()
 
 
-
-plotpolynomial([100,3,0,0])
+quadratic = Polynomial([2,0,4])
+print(quadratic.valueofpolynomial(5),quadratic.degree,quadratic.nameofpolynomial())
+quadratic.plotpolynomial()
