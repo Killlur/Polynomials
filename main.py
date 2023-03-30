177b9c4bcf3e1f4f60bdbdb4eb29a4eca019ebf8
Killlur <kavyamranjan17@gmail.com>
2023-03-30 19:35:44 +0530

    Added projectile and Projectile Projection

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -7,7 +9,6 @@ class Polynomial:
         self.coeffilist=coefflist
     def degreofpolynomial(self):
         return self.degree
-
     def valueofpolynomial(self,x):
         inlist = self.coeffilist
         eqn = 0
@@ -23,9 +24,9 @@ class Polynomial:
             for i in range(self.degree+1):
                 eqn += f'{abs(inlist[i]) if abs(inlist[i]) !=1 else ""}x{self.__get_super(str(self.degree - i))}{"" if inlist[i+1] == (None) else ("-" if abs([i for i in inlist[i:] if i!=0][0]) == [i for i in inlist[i:] if i!=0][0] else "+")}' if (inlist[i] != 0 and self.degree - i != 0) else (f'{inlist[i]}' if inlist[i] !=0 else "")
         return eqn
-    def plotpolynomial(self):
+    def plotpolynomial(self,range=[-100,100]):
 
-        x = np.linspace(-1000, 1000)
+        x = np.linspace(range[0],range[1])
         fig, ax = plt.subplots()
         ax.plot(x, self.valueofpolynomial(x),label=self.nameofpolynomial())
         ax.plot(x,Polynomial([0]).valueofpolynomial(x))
@@ -43,8 +44,26 @@ class Polynomial:
         res = x.maketrans(''.join(normal), ''.join(super_s))
         return x.translate(res)
 
+class Projectile:
+    def __init__(self,u,a,theta):
+        assert u>=0,"Velocity cannot be negative,use the angle from 0 to 360 to do wacky stuff!"
 
-poly1 = Polynomial([0,1,0,9,4])
-print(poly1.valueofpolynomial(5),poly1.degree,poly1.nameofpolynomial())
-poly1.plotpolynomial()
+        self.velocity = u
+        self.gravity=abs(a)
+        self.aop=math.radians(theta)
+
+        a= (-self.gravity)/(2*(self.velocity**2)*(math.cos(self.aop)**2))
+        b=(math.tan(self.aop))
+        self.parabola=Polynomial((a,b,0))
+
+    def Range(self):
+        return ((self.velocity**2) * math.sin(2*self.aop))/self.gravity
+
+    def Projection(self):
+
+        self.parabola.plotpolynomial([-50,50])
+
+tp = Projectile(20,10,225)
+print(tp.Range())
+tp.Projection()
 
