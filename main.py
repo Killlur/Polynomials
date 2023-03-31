30e5bfde7dea093df76aa6db4a5b4514400c1788
Killlur <kavyamranjan17@gmail.com>
2023-03-31 12:00:47 +0530

    Fixed bug in naming

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -22,12 +21,17 @@ class Polynomial:
         else:
             eqn = "y=-" if abs(inlist[0]) != inlist[0] else "y="
             for i in range(self.degree+1):
-                eqn += f'{abs(inlist[i]) if abs(inlist[i]) !=1 else ""}x{self.__get_super(str(self.degree - i))}{"" if ((inlist[i + 1]  is None) or not all(inlist[i:])) else ("-" if abs([g for g in inlist[i:] if g != 0][0]) != [h for h in inlist[i:] if h != 0][0] else "+")}' if (inlist[i] != 0 and self.degree - i != 0) else (f'{inlist[i]}' if inlist[i] != 0 else "")
+                print(inlist[i])
+                eqn += f'{abs(inlist[i]) if abs(inlist[i]) !=1 else ""}x{self.__get_super(str(self.degree - i))}{"" if ((inlist[i +1]  is None) or all(f == 0 for f in inlist[i+1:])) else ("-" if abs([g for g in inlist[i+1:] if g != 0][0]) != [h for h in inlist[i+1:] if h != 0][0] else "+")}' if (inlist[i] != 0 and self.degree - i != 0) else (f'{abs(inlist[i])}' if inlist[i] != 0 else "")
+                print(eqn)
         return eqn
-    def plotpolynomial(self,range=[-100,100]):
+    def plotpolynomial(self, rnge=None):
 
-        x = np.linspace(range[0],range[1])
+        if rnge is None:
+            rnge = [-100, 100]
+        x = np.linspace(rnge[0],rnge[1])
         fig, ax = plt.subplots()
+
         ax.plot(x, self.valueofpolynomial(x),label=self.nameofpolynomial())
         ax.plot(x,Polynomial([0]).valueofpolynomial(x))
         plt.legend()
@@ -52,13 +56,10 @@ class Projectile:
         self.gravity=abs(a)
         self.aop=math.radians(theta)
 
-        a= (-self.gravity)/(2*(self.velocity**2)*(math.cos(self.aop)**2))
-        b=(math.tan(self.aop))
-        print(a,b)
-        self.parabola=Polynomial((a,b,0))
-
-
-
+        a= round((-self.gravity)/(2*(self.velocity**2)*(math.cos(self.aop)**2)),5)
+        b=round((math.tan(self.aop)),5)
+        self.parabola=Polynomial([a,b,0])
+        print(self.parabola.nameofpolynomial())
     def Range(self):
         return ((self.velocity**2) * math.sin(2*self.aop))/self.gravity
 
@@ -66,9 +67,6 @@ class Projectile:
 
         self.parabola.plotpolynomial([-50,50])
 
-tp = Projectile(20,10,225)
-print(tp.Range())
-tp.Projection()
 
 
 
