702ccd402bdab7e1d03ef2691a3553e7098f9bed
Killlur <kavyamranjan17@gmail.com>
2023-04-02 15:32:15 +0530

    Added ability to do input negative velocities with working auto graph

--- main.py
+++ main.py
@@ -50,25 +50,28 @@ class Polynomial:
         return x.translate(res)
 
 class Projectile:
-    def __init__(self,u,a,theta,startheight=0):
-        assert u>=0,"Velocity cannot be negative,use the angle from 0 to 360 to do wacky stuff!"
-
+    def __init__(self,initialvelocity,gravity,theta,startheight=0):
+        #assert initialvelocity>=0,"Velocity cannot be negative,use the angle from -360 to 360 to do wacky stuff!"
+        assert theta > -360 and theta < 360 , "Please Input angle in degrees between -360 to +360"
+        
         self.startheight = startheight
-        self.velocity = u
-        self.gravity=abs(a)
+        self.velocity = initialvelocity
+        self.gravity=abs(gravity)
         self.aop=math.radians(theta)
         self.range = ((self.velocity**2) * math.sin(2*self.aop))/self.gravity
         self.maxheight = ((self.velocity**2)*(math.sin(self.aop)**2))/(2*self.gravity)
 
-        a= round((-self.gravity)/(2*(self.velocity**2)*(math.cos(self.aop)**2)),5)
-        b=round((math.tan(self.aop)),5)
+        self.__privateaop = ((self.aop)+math.pi if (self.aop<math.pi and self.aop>-2*math.pi) else self.aop - math.pi) if self.velocity < 0 else self.aop
+
+        a= round((-self.gravity)/(2*(self.velocity**2)*(math.cos(self.__privateaop)**2)),5)
+        b=round((math.tan(self.__privateaop)),5)
         self.parabola=Polynomial([a,b,startheight])
         print(self.parabola.nameofpolynomial())
     def Range(self):
-        return ((self.velocity**2) * math.sin(2*self.aop))/self.gravity
+        return ((self.velocity**2) * math.sin(2*self.__privateaop))/self.gravity
 
     def MaxHeight(self):
-        return ((self.velocity**2)*(math.sin(self.aop)**2))/(2*self.gravity)
+        return ((self.velocity**2)*(math.sin(self.__privateaop)**2))/(2*self.gravity)
 
     def Projection(self,ylimh=None,xlimh=None,xlinspace=None):
         if ylimh is None:
@@ -80,40 +83,36 @@ class Projectile:
         print(xlimh,ylimh)
         self.parabola.plotpolynomial(xlimh,ylimh,xlinspace,highlightpoint=[0,self.startheight])
     def __autoR(self):
-        print(self.aop)
-        if (self.aop > 0 and self.aop < math.pi/2) or (self.aop < -(3/2)*math.pi and self.aop > -math.pi*2):
+        print(self.__privateaop)
+        if (self.__privateaop > 0 and self.__privateaop < math.pi/2) or (self.__privateaop < -(3/2)*math.pi and self.__privateaop > -math.pi*2):
             return [-5,self.range +10]
-        elif (self.aop > (3/2)*math.pi and self.aop < math.pi*2) or (self.aop < 0 and self.aop > -math.pi/2):
+        elif (self.__privateaop > (3/2)*math.pi and self.__privateaop < math.pi*2) or (self.__privateaop < 0 and self.__privateaop > -math.pi/2):
             return [-5,100]
-        elif (self.aop > math.pi/2 and self.aop < math.pi) or (self.aop > -(3/2)*math.pi and self.aop < -math.pi):
+        elif (self.__privateaop > math.pi/2 and self.__privateaop < math.pi) or (self.__privateaop > -(3/2)*math.pi and self.__privateaop < -math.pi):
             return [self.range -10,5]
-        elif (self.aop > math.pi and self.aop < (3/2)*math.pi) or (self.aop > -math.pi and self.aop < -math.pi/2):
+        elif (self.__privateaop > math.pi and self.__privateaop < (3/2)*math.pi) or (self.__privateaop > -math.pi and self.__privateaop < -math.pi/2):
             return [-100,5]
         else:
             raise "your angle is dumb"
     def __autoLS(self):
-        print(self.aop)
-        if (self.aop > 0 and self.aop < math.pi/2) or (self.aop < -(3/2)*math.pi and self.aop > -math.pi*2):
-            return [0,self.range+(self.startheight*math.tan(self.aop))+5]
-        elif (self.aop > (3/2)*math.pi and self.aop < math.pi*2) or (self.aop < 0 and self.aop > -math.pi/2):
+        print(self.__privateaop)
+        if (self.__privateaop > 0 and self.__privateaop < math.pi/2) or (self.__privateaop < -(3/2)*math.pi and self.__privateaop > -math.pi*2):
+            return [0,self.range+(self.startheight*math.tan(self.__privateaop))+5]
+        elif (self.__privateaop > (3/2)*math.pi and self.__privateaop < math.pi*2) or (self.__privateaop < 0 and self.__privateaop > -math.pi/2):
             return [-5,100]
-        elif (self.aop > math.pi/2 and self.aop < math.pi) or (self.aop > -(3/2)*math.pi and self.aop < -math.pi):
-            return [self.range+(self.startheight*math.tan(self.aop))-5,0]
-        elif (self.aop > math.pi and self.aop < (3/2)*math.pi) or (self.aop > -math.pi and self.aop < -math.pi/2):
+        elif (self.__privateaop > math.pi/2 and self.__privateaop < math.pi) or (self.__privateaop > -(3/2)*math.pi and self.__privateaop < -math.pi):
+            return [self.range+(self.startheight*math.tan(self.__privateaop))-5,0]
+        elif (self.__privateaop > math.pi and self.__privateaop < (3/2)*math.pi) or (self.__privateaop > -math.pi and self.__privateaop < -math.pi/2):
             return [-100,5]
         else:
             raise "your angle is dumb"
     def __autoMH(self):
-        if (self.aop > 0 and self.aop < math.pi) or (self.aop < -math.pi and self.aop > -2*math.pi):
+        if (self.__privateaop > 0 and self.__privateaop < math.pi) or (self.__privateaop < -math.pi and self.__privateaop > -2*math.pi):
             return [self.startheight-5,self.maxheight +20]
-        elif (self.aop > math.pi and self.aop < 2*math.pi) or (self.aop > -math.pi and self.aop < 0):
+        elif (self.__privateaop > math.pi and self.__privateaop < 2*math.pi) or (self.__privateaop > -math.pi and self.__privateaop < 0):
             return [-100,self.startheight+5]
         else:
             raise "your angle is dumb"
 
-
-a = Projectile(30,10,-135,10)
+a = Projectile(initialvelocity=-30,gravity=10,theta=45,startheight=10)
 a.Projection()
-
-
-#y=-0.01111x²-x+10
